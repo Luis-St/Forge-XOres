9c2bad0fe0246e9fb48d1306ff79485bb6236144
BuildTools <76595940+Luis-st@users.noreply.github.com>
2022-01-03 12:19:17 +0100

    deactivate Data Generator

--- src/main/java/net/luis/xores/data/OnGatherDataEvent.java
+++ src/main/java/net/luis/xores/data/OnGatherDataEvent.java
@@ -1,14 +1,6 @@
 package net.luis.xores.data;
 
 import net.luis.xores.XOres;
-import net.luis.xores.data.provider.block.ModBlockStateProvider;
-import net.luis.xores.data.provider.item.ModItemModelProvider;
-import net.luis.xores.data.provider.language.ModLanguageProvider;
-import net.luis.xores.data.provider.loottable.ModLootTableProvider;
-import net.luis.xores.data.provider.recipe.ModRecipeProvider;
-import net.luis.xores.data.provider.tag.ModBlockTagsProvider;
-import net.luis.xores.data.provider.tag.ModItemTagsProvider;
-import net.minecraft.data.DataGenerator;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -19,21 +11,21 @@ public class OnGatherDataEvent {
 	
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent event) {
-		DataGenerator generator = event.getGenerator();
-		if (event.includeDev()) {
-			if (event.includeClient()) {
-				generator.addProvider(new ModBlockStateProvider(generator, event.getExistingFileHelper()));
-				generator.addProvider(new ModItemModelProvider(generator, event.getExistingFileHelper()));
-				generator.addProvider(new ModLanguageProvider(generator));
-			}
-			if (event.includeServer()) {
-				generator.addProvider(new ModLootTableProvider(generator));
-				generator.addProvider(new ModRecipeProvider(generator));
-				ModBlockTagsProvider blockTagsProvider = new ModBlockTagsProvider(generator, event.getExistingFileHelper());
-				generator.addProvider(blockTagsProvider);
-				generator.addProvider(new ModItemTagsProvider(generator, blockTagsProvider, event.getExistingFileHelper()));
-			}
-		}
+//		DataGenerator generator = event.getGenerator();
+//		if (event.includeDev()) {
+//			if (event.includeClient()) {
+//				generator.addProvider(new ModBlockStateProvider(generator, event.getExistingFileHelper()));
+//				generator.addProvider(new ModItemModelProvider(generator, event.getExistingFileHelper()));
+//				generator.addProvider(new ModLanguageProvider(generator));
+//			}
+//			if (event.includeServer()) {
+//				generator.addProvider(new ModLootTableProvider(generator));
+//				generator.addProvider(new ModRecipeProvider(generator));
+//				ModBlockTagsProvider blockTagsProvider = new ModBlockTagsProvider(generator, event.getExistingFileHelper());
+//				generator.addProvider(blockTagsProvider);
+//				generator.addProvider(new ModItemTagsProvider(generator, blockTagsProvider, event.getExistingFileHelper()));
+//			}
+//		}
 	}
 	
 }
