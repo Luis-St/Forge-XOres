5a45351c6c7b16658f3978e225b48564034e71b2
Luis <76595940+Luis-st@users.noreply.github.com>
2022-02-27 13:36:57 +0100

    add XOresElytraLayers to ArmorStandRenderer

--- src/main/java/net/luis/xores/event/registry/OnEntityRenderersEvent.java
+++ src/main/java/net/luis/xores/event/registry/OnEntityRenderersEvent.java
@@ -4,8 +4,10 @@ import net.luis.xores.XOres;
 import net.luis.xores.client.render.layer.XOresElytraLayer;
 import net.luis.xores.common.item.ElytraChestplateItem;
 import net.luis.xores.init.XOresItems;
+import net.minecraft.client.renderer.entity.ArmorStandRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.EntityRenderersEvent.AddLayers;
@@ -34,6 +36,11 @@ public class OnEntityRenderersEvent {
 			playerRenderer.addLayer(new XOresElytraLayer<>(playerRenderer, event.getEntityModels(), XOresItems.ENDERITE_ELYTRA_CHESTPLATE.get(), new ResourceLocation(XOres.MOD_ID, "textures/entity/enderite_elytra.png")));
 			playerRenderer.addLayer(new XOresElytraLayer<>(playerRenderer, event.getEntityModels(), XOresItems.NIGHT_ELYTRA_CHESTPLATE.get(), new ResourceLocation(XOres.MOD_ID, "textures/entity/night_elytra.png")));
 		}
+		ArmorStandRenderer armorStandRenderer = event.getRenderer(EntityType.ARMOR_STAND);
+		armorStandRenderer.addLayer(new XOresElytraLayer<>(armorStandRenderer, event.getEntityModels(), XOresItems.DIAMOND_ELYTRA_CHESTPLATE.get(), new ResourceLocation(XOres.MOD_ID, "textures/entity/diamond_elytra.png")));
+		armorStandRenderer.addLayer(new XOresElytraLayer<>(armorStandRenderer, event.getEntityModels(), XOresItems.NETHERITE_ELYTRA_CHESTPLATE.get(), new ResourceLocation(XOres.MOD_ID, "textures/entity/netherite_elytra.png")));
+		armorStandRenderer.addLayer(new XOresElytraLayer<>(armorStandRenderer, event.getEntityModels(), XOresItems.ENDERITE_ELYTRA_CHESTPLATE.get(), new ResourceLocation(XOres.MOD_ID, "textures/entity/enderite_elytra.png")));
+		armorStandRenderer.addLayer(new XOresElytraLayer<>(armorStandRenderer, event.getEntityModels(), XOresItems.NIGHT_ELYTRA_CHESTPLATE.get(), new ResourceLocation(XOres.MOD_ID, "textures/entity/night_elytra.png")));
 	}
 	
 }
