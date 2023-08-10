197c9f039d8b6ef615277365d1cf5dbd81dee41c
Luis <luis@staudt-fridingen.de>
2023-08-10 17:33:51 +0200

    used static isWearingFullArmor

--- src/main/java/net/luis/xores/world/item/LimoniteArmorItem.java
+++ src/main/java/net/luis/xores/world/item/LimoniteArmorItem.java
@@ -1,10 +1,13 @@
 package net.luis.xores.world.item;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
+import net.luis.xores.world.item.ability.AbilityArmor;
+import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.*;
+import org.jetbrains.annotations.NotNull;
+
+import static net.luis.xores.world.item.ability.AbilityArmor.isWearingFullArmor;
 
 /**
  *
@@ -19,16 +22,21 @@ public class LimoniteArmorItem extends ArmorItem implements AbilityArmor {
 	}
 	
 	@Override
-	public void onItemApplied(LivingEntity entity, EquipmentSlot slot, ItemStack stack) {
-		if (this.isWearingFullArmor(entity)) {
+	public void onItemApplied(@NotNull LivingEntity entity, @NotNull EquipmentSlot slot, @NotNull ItemStack stack) {
+		if (isWearingFullArmor(entity, this.getMaterial())) {
 			entity.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, -1, 1, false, false, false));
 		}
 	}
 	
 	@Override
-	public void onItemRemoved(LivingEntity entity, EquipmentSlot slot, ItemStack stack) {
-		if (!this.isWearingFullArmor(entity)) {
+	public void onItemRemoved(@NotNull LivingEntity entity, @NotNull EquipmentSlot slot, @NotNull ItemStack stack) {
+		if (!isWearingFullArmor(entity, this.getMaterial())) {
 			entity.removeEffect(MobEffects.DIG_SPEED);
 		}
+	}
+	
+	@Override
+	public boolean isAbilityEffect(@NotNull LivingEntity entity, @NotNull MobEffect effect) {
+		return effect == MobEffects.DIG_SPEED;
 	}
 }
--- src/main/java/net/luis/xores/world/item/SaphireArmorItem.java
+++ src/main/java/net/luis/xores/world/item/SaphireArmorItem.java
@@ -1,10 +1,13 @@
 package net.luis.xores.world.item;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
+import net.luis.xores.world.item.ability.AbilityArmor;
+import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.*;
+import org.jetbrains.annotations.NotNull;
+
+import static net.luis.xores.world.item.ability.AbilityArmor.isWearingFullArmor;
 
 /**
  *
@@ -19,18 +22,23 @@ public class SaphireArmorItem extends ArmorItem implements AbilityArmor {
 	}
 	
 	@Override
-	public void onItemApplied(LivingEntity entity, EquipmentSlot slot, ItemStack stack) {
-		if (this.isWearingFullArmor(entity)) {
+	public void onItemApplied(@NotNull LivingEntity entity, @NotNull EquipmentSlot slot, @NotNull ItemStack stack) {
+		if (isWearingFullArmor(entity, this.getMaterial())) {
 			entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, -1, 1, false, false, false));
 			entity.addEffect(new MobEffectInstance(MobEffects.DOLPHINS_GRACE, -1, 0, false, false, false));
 		}
 	}
 	
 	@Override
-	public void onItemRemoved(LivingEntity entity, EquipmentSlot slot, ItemStack stack) {
-		if (!this.isWearingFullArmor(entity)) {
+	public void onItemRemoved(@NotNull LivingEntity entity, @NotNull EquipmentSlot slot, @NotNull ItemStack stack) {
+		if (!isWearingFullArmor(entity, this.getMaterial())) {
 			entity.removeEffect(MobEffects.MOVEMENT_SPEED);
 			entity.removeEffect(MobEffects.DOLPHINS_GRACE);
 		}
+	}
+	
+	@Override
+	public boolean isAbilityEffect(@NotNull LivingEntity entity, @NotNull MobEffect effect) {
+		return effect == MobEffects.MOVEMENT_SPEED || effect == MobEffects.DOLPHINS_GRACE;
 	}
 }
