b8e173c432eae0f7eae209270da1d634f2ad7d5f
Luis <luis@staudt-fridingen.de>
2024-11-13 15:30:17 +0100

    updated elytra chestplate damage timings

---
+++ src/main/java/net/luis/xores/mixin/LivingEntityMixin.java
@@ -0,0 +1,81 @@
+package net.luis.xores.mixin;
+
+import net.luis.xores.world.item.ElytraChestplateItem;
+import net.minecraft.Util;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static net.minecraft.world.entity.LivingEntity.*;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+	
+	//region Mixin
+	@Shadow
+	protected int fallFlyTicks;
+	
+	private LivingEntityMixin(@NotNull EntityType<?> entityType, @NotNull Level level) {
+		super(entityType, level);
+	}
+	
+	@Shadow
+	protected abstract boolean canGlide();
+	
+	@Shadow
+	protected abstract @NotNull ItemStack getItemBySlot(@NotNull EquipmentSlot slot);
+	//endregion
+	
+	@SuppressWarnings("DataFlowIssue")
+	@Inject(method = "updateFallFlying", at = @At("HEAD"), cancellable = true)
+	protected void updateFallFlying(@NotNull CallbackInfo info) {
+		this.checkSlowFallDistance();
+		if (!this.level().isClientSide) {
+			if (!this.canGlide()) {
+				this.setSharedFlag(7, false);
+				return;
+			}
+			int fallFlyTicks = this.fallFlyTicks + 1;
+			Set<EquipmentSlot> slots = EquipmentSlot.VALUES.stream().filter(slot -> canGlideUsing(this.getItemBySlot(slot), slot)).collect(Collectors.toSet());
+			if (slots.contains(EquipmentSlot.CHEST)) {
+				ItemStack itemStack = this.getItemBySlot(EquipmentSlot.CHEST);
+				int damageTimeValue = 20;
+				if (itemStack.getItem() instanceof ElytraChestplateItem item) {
+					damageTimeValue = ElytraChestplateItem.getDamageTimeValue(item);
+				}
+				if (fallFlyTicks % 10 == 0) {
+					this.gameEvent(GameEvent.ELYTRA_GLIDE);
+				}
+				if (fallFlyTicks % damageTimeValue == 0) {
+					itemStack.hurtAndBreak(1, (LivingEntity) (Object) this, EquipmentSlot.CHEST);
+				}
+			} else {
+				EquipmentSlot slot = Util.getRandom(List.copyOf(slots), this.random);
+				if (fallFlyTicks % 10 == 0) {
+					if ((fallFlyTicks / 10) % 2 == 0) {
+						this.getItemBySlot(slot).hurtAndBreak(1, (LivingEntity) (Object) this, slot);
+					}
+					this.gameEvent(GameEvent.ELYTRA_GLIDE);
+				}
+			}
+		}
+		info.cancel();
+	}
+}
--- src/main/java/net/luis/xores/world/item/ElytraChestplateItem.java
+++ src/main/java/net/luis/xores/world/item/ElytraChestplateItem.java
@@ -21,16 +21,12 @@ package net.luis.xores.world.item;
 import com.google.common.collect.Maps;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.util.Unit;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.equipment.ArmorType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
-import java.util.Random;
 
 /**
  *
@@ -41,7 +37,6 @@ import java.util.Random;
 public class ElytraChestplateItem extends ArmorItem {
 	
 	private static final Map<ElytraChestplateItem, Integer> ELYTRA_TIME_DAMAGE_VALUES = Maps.newHashMap();
-	private static final Random RNG = new Random(System.currentTimeMillis());
 	
 	public ElytraChestplateItem(@NotNull ArmorMaterial material, @NotNull Properties properties) {
 		super(material, ArmorType.CHESTPLATE, properties.component(DataComponents.GLIDER, Unit.INSTANCE));
@@ -51,11 +46,7 @@ public class ElytraChestplateItem extends ArmorItem {
 		ELYTRA_TIME_DAMAGE_VALUES.put(elytraChestplate, damageValue);
 	}
 	
-	@Override
-	public boolean elytraFlightTick(@NotNull ItemStack stack, @NotNull LivingEntity entity, int flightTicks) {
-		if (!entity.level().isClientSide && (flightTicks + 1) % ELYTRA_TIME_DAMAGE_VALUES.getOrDefault(this, 20) == 0) {
-			stack.hurtAndBreak(1, entity, EquipmentSlot.CHEST);
-		}
-		return true;
+	public static int getDamageTimeValue(@NotNull ElytraChestplateItem elytraChestplate) {
+		return ELYTRA_TIME_DAMAGE_VALUES.getOrDefault(elytraChestplate, 20);
 	}
 }
